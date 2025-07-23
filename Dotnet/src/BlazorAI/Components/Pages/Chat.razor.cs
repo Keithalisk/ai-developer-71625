5d0843d0a3942b63e79856b19a5bbe9f10f0a946
Keithalisk <keithalisk@gmail.com>
2025-07-23 09:10:31 -0400

    challenge 1 completed

--- Dotnet/src/BlazorAI/Components/Pages/Chat.razor.cs
+++ Dotnet/src/BlazorAI/Components/Pages/Chat.razor.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using ModelContextProtocol;
+using ModelContextProtocol.Client;
 
 #pragma warning disable SKEXP0040 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 #pragma warning disable SKEXP0020 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
@@ -13,6 +17,8 @@ public partial class Chat
 {
     private ChatHistory? chatHistory;
     private Kernel? kernel;
+    private IMcpClient? mcpClient;
+    private OpenAIPromptExecutionSettings openAIPromptExecutionSettings;
 
     [Inject]
     public required IConfiguration Configuration { get; set; }
@@ -38,23 +44,42 @@ public partial class Chat
         // Challenge 03 and 04 - Services Required
         kernelBuilder.Services.AddHttpClient();
 
-        // Challenge 05 - Register Azure AI Foundry Text Embeddings Generation
+        // Challenge 04 - create an MCP client for the MS Learn Server
+        // Create an MCPClient for the MSLearn server
 
+        // Challenge 05 - Register Azure AI Foundry Text Embeddings Generation
 
         // Challenge 05 - Register Search Index
 
-
         // Challenge 07 - Add Azure AI Foundry Text To Image
-
 
         // Challenge 02 - Finalize Kernel Builder
         kernel = kernelBuilder.Build();
+
+        // Challenge 04 - add a client for the MSLearn MCP Server
+        /*
+        if (mcpClient != null)
+        {
+            await mcpClient.DisposeAsync();
+            mcpClient = null;
+        }
+        mcpClient = await McpClientFactory.CreateAsync(new SseClientTransport(new SseClientTransportOptions
+        {
+            Endpoint = new Uri(Configuration.GetValue<string>("MCP_ENDPOINT") ??
+                               throw new Exception("MCP_ENDPOINT is not set in configuration")),
+        }));
+        */
 
         // Challenge 03, 04, 05, & 07 - Add Plugins
         await AddPlugins();
 
         // Challenge 03 - Create OpenAIPromptExecutionSettings
-
+        openAIPromptExecutionSettings = new()
+        {
+            FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(),
+            ChatSystemPrompt = "You are an AI assistant that helps people find information.  Ask follow-up questions if something is unclear or you need more information to complete a task.",
+            Temperature = 0.9f
+        };
 
     }
 
@@ -64,6 +89,13 @@ public partial class Chat
         // Challenge 03 - Add Time Plugin
 
         // Challenge 04 - Import OpenAPI Spec
+
+
+        // Challenge 04 - Add the MCP Server tools
+        /*
+        var tools = await mcpClient.ListToolsAsync().ConfigureAwait(false);
+        kernel.Plugins.AddFromFunctions("MSLearn", tools.Select(aiFunction => aiFunction.AsKernelFunction()));
+        */
 
         // Challenge 05 - Add Search Plugin
 
@@ -84,13 +116,29 @@ public partial class Chat
             StateHasChanged();
 
             // Challenge 02 - Retrieve the chat completion service
+            var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 
             // Challenge 02 - Update Chat History
+            chatHistory.AddUserMessage(userMessage);
 
-            // Challenge 02 - Send a message to the chat completion service
+            try
+            {
+                // Challenge 02 - Send a message to the chat completion service
+                var response = await chatCompletionService.GetChatMessageContentsAsync(
+                    chatHistory,
+                    executionSettings: openAIPromptExecutionSettings,
+                    kernel: kernel);
 
-            // Challenge 02 - Add Response to the Chat History object
-
+                // Challenge 02 - Add Response to the Chat History object
+                chatHistory.AddRange(response);
+            }
+            catch (HttpOperationException e)
+            {
+                if (e.ResponseContent != null)
+                {
+                    chatHistory.AddAssistantMessage(e.ResponseContent);
+                }
+            }
 
             loading = false;
         }
